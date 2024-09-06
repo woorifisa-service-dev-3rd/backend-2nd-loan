a2fc11dd26683d35c3fbc9ccd94d24d34815e636
YE-EUN <musiclove529@naver.com>
2024-09-06 14:43:17 +0900

    # 제목 (Type: Subject) feat: init
    ##############################
    # 본문 (Body)
    기본 클래스 생성
    
    ##############################
    # 꼬리말 (Footer)
    issue #1
    
    ##############################
    # feat : 새로운 기능을 추가하는 경우
    # fix : 버그를 고친경우
    # docs : 문서를 수정한 경우
    # style : 코드 포맷 변경, 세미콜론 누락, 코드 수정이 없는경우
    # refact : 코드 리펙토링
    # test : 테스트 코드. 리펙토링 테스트 코드를 추가했을 때
    # chore : 빌드 업무 수정, 패키지 매니저 수정
    # design : CSS 등 사용자가 UI 디자인을 변경했을 때
    # rename : 파일명(or 폴더명) 을 수정한 경우
    # remove : 코드(파일) 의 삭제가 있을 때

---
+++ loan/src/main/java/dev/service/cloud/loan/controller/LoanController.java
@@ -0,0 +1,11 @@
+package dev.service.cloud.loan.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/loans")
+@RequiredArgsConstructor
+public class LoanController {
+}
