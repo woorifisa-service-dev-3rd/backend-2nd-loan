61e761b98482ec0334f8df4aae1101c88da75da6
YE-EUN <musiclove529@naver.com>
2024-09-06 16:57:41 +0900

    rename: LongProductRepository 이름 수정
    issue #1

--- loan/src/main/java/dev/service/cloud/loan/repository/LoanProductRepository.java
+++ loan/src/main/java/dev/service/cloud/loan/repository/LoanProductRepository.java
@@ -3,5 +3,5 @@ package dev.service.cloud.loan.repository;
 import dev.service.cloud.loan.model.LoanProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LoanProductController extends JpaRepository<LoanProduct, Long> {
+public interface LoanProductRepository extends JpaRepository<LoanProduct, Long> {
 }
