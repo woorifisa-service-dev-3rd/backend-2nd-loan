106ca060f9197762f85d911b6a22a6800975c8bf
dlaudfuf33 <dlaudfuf33@gmail.com>
2024-09-07 14:48:36 +0900

    refact : 오타 수정

--- loan/src/main/java/dev/service/cloud/loan/repository/LoanProductRepository.java
+++ loan/src/main/java/dev/service/cloud/loan/repository/LoanProductRepository.java
@@ -13,12 +13,12 @@ public interface LoanProductRepository extends JpaRepository<LoanProduct, Long> 
      * 전달받은 점수를 기준으로 신청 가능한 상품중 대출중인 상품들을
      * 최대한도 오름차순, 이자율 오름차순으로 정렬헤서 꺼내온다
      *
-     * @param creditScore
-     * @return
+     * @param point
+     * @return List<LoanProduct> sorted
      */
     @Query(value = "SELECT * FROM loan_products lp WHERE lp.required_credit_score <= :creditScore AND CURRENT_DATE <= lp.end_date ORDER BY lp.max_limit ASC, lp.interest_rate ASC LIMIT 10",
             nativeQuery = true)
-    List<LoanProduct> findEligibleLoanProducts(@Param("creditScore") int creditScore);
+    List<LoanProduct> findEligibleLoanProducts(@Param("creditScore") int point);
 
     /**
      * 회원 대출 이력이 없는 경우 신용점수 기준으로 상품을 추천
