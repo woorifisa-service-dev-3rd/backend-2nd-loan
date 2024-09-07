0fa34e5575478b58b3d2d4c53e8b40d2f5cee829
dlaudfuf33 <dlaudfuf33@gmail.com>
2024-09-07 17:39:18 +0900

    feature : MemberController.getMemberLoanlist 구현

--- loan/src/main/java/dev/service/cloud/loan/controller/MemberController.java
+++ loan/src/main/java/dev/service/cloud/loan/controller/MemberController.java
@@ -1,11 +1,29 @@
 package dev.service.cloud.loan.controller;
 
+import dev.service.cloud.loan.dto.response.LoanResponseDto;
+import dev.service.cloud.loan.service.LoanService;
+import dev.service.cloud.loan.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/members")
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
+    private final MemberService memberService;
+    private final LoanService loanService;
+
+    @RequestMapping("/{id}")
+    public List<LoanResponseDto> getMemberLoanlist(@PathVariable("id") Long memberId, Model model) {
+        loanService.getLoanListByMemberId(memberId);
+        return List.of();
+    }
 }
