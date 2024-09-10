75fa7e526e1721c291b32bb9be2c84ec047bdadf
dlaudfuf33 <dlaudfuf33@gmail.com>
2024-09-10 19:45:49 +0900

    feature : ErrorCode.java 커스텀예외 추가

--- loan/src/main/java/dev/service/cloud/loan/exception/ErrorCode.java
+++ loan/src/main/java/dev/service/cloud/loan/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     LOAN_PRODUCT_NOT_FOUND("해당하는 대출 상품이 없습니다."),
     LOAN_CONDITION_NOT_MATCH("대출 조건이 맞지 않습니다"),
     LOW_CREDIT_SCORE("신용 점수가 낮습니다."),
-    OVER_MAX_LIMIT_LOAN_AMOUNT("최대 대출 한도를 넘었습니다.");
+    OVER_MAX_LIMIT_LOAN_AMOUNT("최대 대출 한도를 넘었습니다."),
+    RECOMMEND_NOT_FOUND("추천에 적합한 상품이 없습니다.");
 
     private String message;
 }
