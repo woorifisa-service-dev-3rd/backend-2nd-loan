32c6c9b4ad88a193991bf7054a7151c9f9b05efd
3-34\3-34 <cbj0010@naver.com>
2024-09-11 16:38:51 +0900

    feat : loan DTO rename

--- loan/src/main/java/dev/service/cloud/loan/dto/response/LoanProductResponseDto.java
+++ loan/src/main/java/dev/service/cloud/loan/dto/response/LoanProductResponseDto.java
@@ -12,28 +12,35 @@ import java.math.BigDecimal;
 @ToString
 public class LoanProductResponseDto {
 
-    private LoanProductsType loanProductsType;  //대출유형
+    private String loanProductsType;  //대출유형
     private BigDecimal interestRate;    //이자율
     private Integer maxLimit;   //대출한도
     private Long repaymentPeriod;  //상환기간
-    private LoanProductsFeature loanProductsFeature;    //상품특징Id
-    private ApplicationMethod applicationMethod;    //대출신청방법
+    private String loanProductsFeature;    //상품특징Id에 대한 이름
+    private String applicationMethod;    //대출신청방법 id에 대한 대출신청방법
     private Integer requiredCreditScore;
-    private Provider provider;
+    private String provider;
 
 
     public static LoanProductResponseDto from(LoanProduct loanProduct) {
         String featureName = loanProduct.getLoanProductsFeature().getName();
+        LoanProductsFeature loanProductsFeature = loanProduct.getLoanProductsFeature();
+        ApplicationMethod applicationMethod = loanProduct.getApplicationMethod();
+        LoanProductsType loanProductsType = loanProduct.getLoanProductsType();
+        Provider provider = loanProduct.getProvider();
+
         System.out.println("featureName = " + featureName);
+        System.out.println("loanProductsFeature = " + loanProductsFeature);
 
         return LoanProductResponseDto.builder()
-                .loanProductsType(loanProduct.getLoanProductsType())
+                .loanProductsType(loanProductsType.getName())
                 .interestRate(loanProduct.getInterestRate())
+                .loanProductsFeature(loanProductsFeature.getName())
                 .maxLimit(loanProduct.getMaxLimit())
                 .repaymentPeriod(loanProduct.getRepaymentPeriod())
-                .applicationMethod(loanProduct.getApplicationMethod())
+                .applicationMethod(String.valueOf(applicationMethod.getLoanProducts()))
                 .requiredCreditScore(loanProduct.getRequiredCreditScore())
-                .provider(loanProduct.getProvider())
+                .provider(provider.getName())
                 .build();
     }
 }
