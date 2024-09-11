d57f5ed7169b93c94bcc518b99b47f006c308d93
dlaudfuf33 <dlaudfuf33@gmail.com>
2024-09-11 10:30:10 +0900

    feat:회원의 대출 목록 조회

--- loan/src/main/java/dev/service/cloud/loan/controller/MemberController.java
+++ loan/src/main/java/dev/service/cloud/loan/controller/MemberController.java
@@ -5,8 +5,8 @@ import dev.service.cloud.loan.service.LoanService;
 import dev.service.cloud.loan.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-//@RestController
-@Controller
+@RestController
 @RequestMapping("/members")
 @RequiredArgsConstructor
 @Slf4j
@@ -23,16 +22,14 @@ public class MemberController {
     private final MemberService memberService;
     private final LoanService loanService;
 
-
     /**
      * 회원의 대출 목록 조회
      * @param memberId
-     * @param model
      * @return List<LoanResponseDto> 해당 회원의 대출 목록 반환
      */
     @GetMapping("{id}/loans")
-    public String getMemberLoanlist(@PathVariable("id") Long memberId, Model model) {
-        model.addAttribute("loanList",loanService.getLoanListByMemberId(memberId));
-        return "memeber_loanList";
+    public ResponseEntity<List<LoanResponseDto>> getMemberLoanlist(@PathVariable("id") Long memberId) {
+        List<LoanResponseDto> resDTO =loanService.getLoanListByMemberId(memberId);
+        return new ResponseEntity<>(resDTO, HttpStatus.OK);
     }
 }
