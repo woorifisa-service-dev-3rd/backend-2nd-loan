6e8dc7fcb2f1bf32d0e8070e890f931c3518fa03
dlaudfuf33 <dlaudfuf33@gmail.com>
2024-09-12 10:23:41 +0900

    refact : 대출이력 조회에 필요한 필드 및 메소드 추가

--- loan/src/main/java/dev/service/cloud/loan/dto/response/LoanResponseDto.java
+++ loan/src/main/java/dev/service/cloud/loan/dto/response/LoanResponseDto.java
@@ -7,6 +7,7 @@ import dev.service.cloud.loan.model.MemberLoanProduct;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -15,15 +16,24 @@ import java.util.stream.Collectors;
 @Builder
 @Getter
 @ToString
+@Slf4j
 public class LoanResponseDto {
+    private Long id;
+    private Long memberId;
     private String memberName;
-    private String loanProductTypeName;
-    private LocalDate startDate;
-    private Long loanAmount;
-    private LocalDate loanDueDate;
 
     private String providerName;
+    private String loanProductTypeName;
+    private LocalDate startDate;
+    private LocalDate loanDueDate;
+    private Long loanAmount; // 대출금
+    private Long goalAmount; // 매달 상환해야 할 금액
+    private Long totalPaidAmount; // 여태 상환액
+    private Long totalRepaymentAmount; // 대출금 + 이자
+
     private int repaymentCount;
+    private int latePayment;
+
 
     public static LoanResponseDto toDto(MemberLoanProduct memberLoanProduct) {
         Member member = memberLoanProduct.getMember();
@@ -48,22 +58,35 @@ public class LoanResponseDto {
                 .collect(Collectors.toList());
     }
 
-    public static LoanResponseDto toDto4membersLoanlist(MemberLoanProduct memberLoanProduct) {
+
+    public static LoanResponseDto toHistoryDto(MemberLoanProduct memberLoanProduct) {
         Member member = memberLoanProduct.getMember();
-        System.out.println("member = " + member);
+        log.debug("member = {}", member);
         LoanProduct loanProduct = memberLoanProduct.getLoanProduct();
-        System.out.println("loanProduct = " + loanProduct);
+        log.debug("loanProduct = {}", loanProduct);
         LoanProductsType loanProductsType = loanProduct.getLoanProductsType();
-        System.out.println("loanProductsType = " + loanProductsType);
+        log.debug("loanProductsType = {}", loanProductsType);
+
 
         return LoanResponseDto.builder()
+                .id(memberLoanProduct.getId())
+                .memberId(member.getId())
                 .memberName(member.getName())
                 .loanProductTypeName(loanProductsType.getName())
-                .startDate(memberLoanProduct.getStartDate())
-                .loanAmount(memberLoanProduct.getLoanAmount())
-                .loanDueDate(memberLoanProduct.getLoanDueDate())
-                .repaymentCount(memberLoanProduct.getRepaymentCount())
                 .providerName(loanProduct.getProvider().getName())
+                .startDate(memberLoanProduct.getStartDate())
+                .loanDueDate(memberLoanProduct.getLoanDueDate())
+                .loanAmount(memberLoanProduct.getLoanAmount())
+                .goalAmount(memberLoanProduct.getGoalAmount())
+                .totalPaidAmount(memberLoanProduct.getTotalPaidAmount())
+                .totalRepaymentAmount(memberLoanProduct.getTotalRepaymentAmount())
+                .repaymentCount(memberLoanProduct.getRepaymentCount())
+                .latePayment(memberLoanProduct.getLatePaymentCount())
                 .build();
+
+    }
+
+    public static List<LoanResponseDto> toHistoryDtos(List<MemberLoanProduct> memberLoanProducts) {
+        return memberLoanProducts.stream().map(LoanResponseDto::toHistoryDto).collect(Collectors.toList());
     }
 }
