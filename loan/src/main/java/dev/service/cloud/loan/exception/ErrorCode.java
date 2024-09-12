2c130dc844274cb209d0c8b7d4e6e1e8394fd73e
dlaudfuf33 <dlaudfuf33@gmail.com>
2024-09-12 10:15:46 +0900

    refact : Enum 추가 -> LOAN_HISTORY_NOT_FOUND

--- loan/src/main/java/dev/service/cloud/loan/exception/ErrorCode.java
+++ loan/src/main/java/dev/service/cloud/loan/exception/ErrorCode.java
@@ -8,12 +8,13 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     MEMBER_NOT_FOUND("해당하는 회원이 없습니다."),
+    RECOMMEND_NOT_FOUND("추천에 적합한 상품이 없습니다."),
     LOAN_PRODUCT_NOT_FOUND("해당하는 대출 상품이 없습니다."),
+    LOAN_HISTORY_NOT_FOUND("대출 이력이 없습니다."),
     LOAN_CONDITION_NOT_MATCH("대출 조건이 맞지 않습니다"),
     LOW_CREDIT_SCORE("신용 점수가 낮습니다."),
     OVER_MAX_LIMIT_LOAN_AMOUNT("최대 대출 한도를 넘었습니다."),
-    WRONG_CREDIT_SCORE("유효하지 않은 신용점수"),
-    RECOMMEND_NOT_FOUND("추천에 적합한 상품이 없습니다.");
+    WRONG_CREDIT_SCORE("유효하지 않은 신용점수");
 
     private String message;
 }
